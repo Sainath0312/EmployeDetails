46df0211f1db5dff44294a6240df451b444d768d
Sainath <134257519+Sainath0312@users.noreply.github.com>
2023-05-25 14:47:18 +0530

    Update DataBaseConnector.java

--- UsingContextXML/src/main/java/AllMethods/DataBaseConnector.java
+++ UsingContextXML/src/main/java/AllMethods/DataBaseConnector.java
@@ -2,7 +2,7 @@ package AllMethods;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.ResultSet; 
 import java.util.ArrayList;
 import java.util.List;
 
